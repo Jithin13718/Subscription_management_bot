d992408377803613ddbf13b7eed008333db1f061
Jithin13718 <jpentako3@gitam.in>
2026-02-06 18:06:09 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,22 @@
+// config.js
+const fs = require("fs");
+const path = require("path");
 
+function loadConfig(env = process.env.NODE_ENV || "development") {
+  const configPath = path.join(__dirname, "..", "config", `${env}.json`);
+  try {
+    const rawData = fs.readFileSync(configPath);
+    const config = JSON.parse(rawData);
+    return config;
+  } catch (err) {
+    console.error(`Error loading config for ${env}:`, err);
+    return {};
+  }
+}
+
+module.exports = { loadConfig };
+
+// Example usage:
+// const { loadConfig } = require("./utils/config");
+// const config = loadConfig();
+// console.log("Loaded config:", config);
